4df5ec3d07947473dbb3379f1f88f13e75a28076
ZlyCzerw <wiktor.marc@gmail.com>
2019-01-17 10:13:19 +0100

    17.01.19

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -36,11 +36,9 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getRecieverEmail());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-        if (mail.getToCc()!=null) {
-            mailMessage.setCc(mail.getToCc());
-        }
-        return mailMessage;
+        Optional.ofNullable(mail.getToCc()).ifPresent(cc -> mailMessage.setCc(cc));
 
+       return mailMessage;
     }
 
 
