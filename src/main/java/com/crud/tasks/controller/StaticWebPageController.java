0943c7975d911a816606a624dd5338d508a667c3
ZlyCzerw <wiktor.marc@gmail.com>
2019-01-29 11:54:27 +0100

    thymeleaf2

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -4,12 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Map;
+
 @Controller
 public class StaticWebPageController {
 
 
     @RequestMapping("/")
-    public String index(){
+    public String index(Map<String,Object> model){
+        model.put("variable", "My Thymeleaf variable");
+        model.put("one",1);
+        model.put("two",2);
+        model.put("three",3);
+        model.put("times", "*");
+        model.put("minus", "-");
+        model.put("plus", "+");
+        model.put("equals", "=");
         return "index";
     }
 }
